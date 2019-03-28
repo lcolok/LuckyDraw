1a6ae4840ab8ecc8a072e65a97ea26a5ca788fdd
lcolok <425311101@qq.com>
2019-03-29 00:47:13 +0800

    把公司做的工程补回来了

--- js/index.js
+++ js/index.js
@@ -2,26 +2,47 @@
 相关设置分离到settings.js
 */
 //未中奖人员名单
-var remainPerson = allPerson.toString().split(";");
+var remainPerson = allPerson;
 //中奖人员名单
 var luckyMan = [];
 var timer;//定时器
 var times = 1;//抽奖次数,如果不是第一次，不加粗显示领导姓名
 
+var pool = [
+    [
+        {
+            name: "逗号咖啡券",
+            quota: 8
+        }, {
+            name: "家用油壶",
+            quota: 10
+        }, {
+            name: "家有健康空气检测1次",
+            quota: 2
+        }, {
+            name: "水杯",
+            quota: 8
+        }, {
+            name: "星乐杯",
+            quota: 2
+        }
+    ]
+]
 
 $(function () {
     iconAnimation();
     //开始抽奖
-    $("#btnStart").text("开始"+"　（共"+remainPerson.length+"人）");//设置按钮文本为开始
+    $("#txtNum").val(1);
+    $("#btnStart").text("开始");//设置按钮文本为开始
     $("#btnStart").on("click", function () {
         //判断是开始还是结束
-        if ($("#btnStart").text().substring(0,2) === "开始") {
+        if ($("#btnStart").text().substring(0, 2) === "开始") {
             if (!$("#txtNum").val()) {
-                showDialog("请输入中奖人数");
+                showDialog("请输入第几轮");
                 return false;
             }
-            if ($("#txtNum").val() > 49) {
-                showDialog("一次最多只能输入49人");
+            if ($("#txtNum").val() > pool.length || $("#txtNum").val() <= 0) {
+                showDialog(`只能输入1${pool.length > 1 ? '~' + pool.length : ''}`);
                 return false;
             }
             if ($("#txtNum").val() > remainPerson.length) {
@@ -38,16 +59,16 @@ $(function () {
         else {
             //PillarsZhang：剩余人数我加的，这边用了全角空格
             var luckyDrawNum = $("#txtNum").val();
-            startLuckDraw();//抽奖开始
+            startLuckDraw(pool);//抽奖开始
 
             $("#luckyDrawing").fadeOut();
             clearInterval(timer);//停止输入框动画展示
             $("#luckyDrawing").val(luckyMan[luckyMan.length - 1]);//输入框显示最后一个中奖名字
-            $("#result").fadeIn().find("div").removeClass().addClass("p" + luckyDrawNum);//隐藏输入框，显示中奖框
+            $("#result").fadeIn().find("div").removeClass().addClass("p" + 50);//隐藏输入框，显示中奖框  p后面的数字决定了字体大小
             $("#bgLuckyDrawEnd").addClass("bg");//添加中奖背景光辉
             $("#txtNum").attr("placeholder", "输入中奖人数(" + remainPerson.length + ")");
-            $("#btnStart").text("开始"+"　（剩余"+remainPerson.length+"人）");//设置按钮文本为开始
-    }
+            $("#btnStart").text("开始");//设置按钮文本为开始
+        }
     });
 
     $("#btnReset").on("click", function () {
@@ -57,15 +78,15 @@ $(function () {
             //熏置未中奖人员名单
             remainPerson = allPerson.toString().split(";");
             //中奖人数框置空
-            $("#txtNum").val("").attr("placeholder", "请输入中奖人数");
+            $("#txtNum").val("").attr("placeholder", "请输入第几轮");
             $("#showName").val("");
             //隐藏中奖名单,然后显示抽奖框
-            $("#result").fadeOut("normal",function(){
+            $("#result").fadeOut("normal", function () {
                 $("#result").html("<div><font size=\"10\">准备就绪</font></div>");
                 $("#result").fadeIn();
-                });//动画效果过渡成准备就绪（PillarsZhang）
+            });//动画效果过渡成准备就绪（PillarsZhang）
             $("#bgLuckyDrawEnd").removeClass("bg");//移除背景光辉
-            $("#btnStart").text("开始"+"　（共"+remainPerson.length+"人）");//设置按钮文本为开始
+            $("#btnStart").text("开始");//设置按钮文本为开始
             times++;
             console.log(times);
 
@@ -74,36 +95,54 @@ $(function () {
 });
 
 //抽奖主程序
-function startLuckDraw() {
+function startLuckDraw(pool) {
     //抽奖人数
-    var luckyDrawNum = $("#txtNum").val();
-    if (luckyDrawNum > remainPerson.length) {
-        alert("抽奖人数大于奖池人数！请修改人数。或者点重置开始将新一轮抽奖！");
-        return false;
-    }
-    //随机中奖人
-    var randomPerson = getRandomArrayElements(remainPerson, luckyDrawNum);
-    var tempHtml = "";
-    $.each(randomPerson, function (i, person) {
-        if (leaderArr.indexOf(person) > -1 && times == 1) {
-            tempHtml += "<span><b>" + person + "</b></span>";
-        }
-        else {
-            tempHtml += "<span>" + person + "</span>";
-        }
-    });
-    $("#result>div").html(tempHtml);
-    //剩余人数剔除已中奖名单
-    remainPerson = remainPerson.delete(randomPerson);
-    //中奖人员
-    luckyMan = luckyMan.concat(randomPerson);
-    //设置抽奖人数框数字为空
+    var round = $("#txtNum").val();
+    /*     if (luckyDrawNum > remainPerson.length) {
+            alert("抽奖人数大于奖池人数！请修改人数。或者点重置开始将新一轮抽奖！");
+            return false;
+        } */
 
-    if (setEmptyPerson) { $("#txtNum").val("");};
-    /*
-    PillarsZhang补充：有时候不需要每次都把人数框清空
-    根据需求来吧，在settings.js里改
-    */
+    var itemArr = pool[round - 1];
+    var randomPerson;
+
+    itemArr.forEach(e => {
+        e.name
+
+
+
+
+
+
+        //随机中奖人
+        randomPerson = getRandomArrayElements(remainPerson, e.quota);
+
+        var tempHtml = "<span>" + e.name + "</span>";
+        $.each(randomPerson, function (i, person) {
+            /* if (leaderArr.indexOf(person) > -1 && times == 1) {
+                tempHtml += "<span><b>" + person + "</b></span>";
+            }
+            else {
+                tempHtml += "<span>" + person + "</span>";
+            } */
+
+            tempHtml += "<span>" + person + "</span>";
+        });
+        $("#result>div").html(tempHtml);
+        //剩余人数剔除已中奖名单
+        remainPerson = remainPerson.delete(randomPerson);
+        //中奖人员
+        luckyMan = luckyMan.concat(randomPerson);
+        //设置抽奖人数框数字为空
+
+        if (setEmptyPerson) { $("#txtNum").val(""); };
+        /*
+        PillarsZhang补充：有时候不需要每次都把人数框清空
+        根据需求来吧，在settings.js里改
+        */
+
+    });
+
 
 }
 
